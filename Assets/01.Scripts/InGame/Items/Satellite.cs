28112f1c3d191ba18ae75c536b8afc48e507c4d2
Omarchoi <omarchoi80@gmail.com>
2026-01-27 15:46:14 +0900

    feat: 위성 레이저 구현

--- Assets/01.Scripts/InGame/Items/Satellite.cs
+++ Assets/01.Scripts/InGame/Items/Satellite.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
 
+public enum ESatelliteType
+{
+    Beam,
+    Projectile
+}
+
 public class Satellite : MonoBehaviour
 {
+    [Header("Satellite")]
     [SerializeField] private Transform _parent;
     [SerializeField] private float _orbitSpeed = 50f;
     [SerializeField] private float _orbitDistance = 1.65f;
+    
+    [Space(10)]
+    [Header("Beam")]
+    [SerializeField] private LineRenderer _beam;
+    [SerializeField] private float _beamDistance = 1f;
+    [SerializeField] private int _beamSegments = 20;
+    [SerializeField] private float _beamSpace = 0.25f;
+    [SerializeField] private float _waveAmplitude = 0.1f;
+    [SerializeField] private float _waveFrequency = 5f;
+    [SerializeField] private float _waveSpeed = 5f;
+    
     private float _angle;
     
     private void Start()
@@ -18,12 +36,35 @@ public class Satellite : MonoBehaviour
         
         UpdatePosition();
         UpdateRotation();
+        UpdateBeam();
     }
     
+    private void UpdateBeam()
+    {
+        if (_beam == null) return;
+
+        _beam.positionCount = _beamSegments;
+
+        Vector3 startPos = new Vector3(_beamSpace, 0, 0);
+        Vector3 endPos = new Vector3(_beamDistance, 0, 0);
+
+        for (int i = 0; i < _beamSegments; i++)
+        {
+            float t = i / (float)(_beamSegments - 1);
+
+            Vector3 basePos = Vector3.Lerp(startPos, endPos, t);
+            float wave = Mathf.Sin(t * _waveFrequency * Mathf.PI + Time.time * _waveSpeed) * _waveAmplitude;
+
+            basePos.y += wave;
+
+            _beam.SetPosition(i, basePos);
+        }
+    }
+
     private void UpdateRotation()
     {
-        Vector3 direction = _parent.position - transform.position;
-        float angleToParent = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Vector3 direction = _parent.position - transform.position;                                 
+        float angleToParent = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;               
         transform.rotation = Quaternion.Euler(0, 0, angleToParent);
     }
 
