aa282a3eb9cc57a1e9d2f9930bb16946403d8395
Omarchoi <omarchoi80@gmail.com>
2026-01-30 14:13:19 +0900

    feat: Account Domain 추가

---
+++ Assets/01.Scripts/OutGame/Feature/Account/2.Domain/Account.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Text.RegularExpressions;
+
+public class Account
+{
+    public readonly string Email;
+    public readonly string Password;
+    
+    private const string EmailPattern =
+        @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+    private const string PasswordPattern =
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])[A-Za-z\d\W_]{7,20}$";
+    
+    public Account(string email, string password)
+    {
+        if (string.IsNullOrEmpty(email)) throw new System.ArgumentException("[Account.cs] Email cannot be null or empty.");
+        if (!Regex.IsMatch(email, EmailPattern)) throw new ArgumentException("[Account.cs] Email must be a valid email address.");
+        if (string.IsNullOrEmpty(password)) throw new ArgumentException("[Account.cs] Password cannot be null or empty.");
+        if (!Regex.IsMatch(password, PasswordPattern)) throw new ArgumentException("[Account.cs] Password must be a valid password.");
+        
+        Email = email;
+        Password = password;
+    }
+}
+
+// 이메일 규칙
+// 비어있으면 안된다.
+// 올바른 이메일이어야 한다.
+// 동일한 이메일으로 중복 가입이 불가능하다.
+//
+// 비밀번호 규칙
+// 비어있으면 안된다.
+// 6자리 이상 12자 이하여야 한다.
+// 대문자를 1개 이상 포함해야 한다.
+// 특수문자를 1개 이상 포함해야 한다.
