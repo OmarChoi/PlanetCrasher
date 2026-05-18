1bf49c61dadbbd6543d9666fedc451d0beae2967
OmarChoi <omarchoi80@gmail.com>
2026-05-19 03:54:34 +0900

    refactor: 클릭 Scale Feedback 제공시 Tween을 새로 생성하지 않고 재사용하게 변경

--- Assets/01.Scripts/InGame/Feedback/ScaleTweeningFeedback.cs
+++ Assets/01.Scripts/InGame/Feedback/ScaleTweeningFeedback.cs
@@ -6,12 +6,32 @@ public class ScaleTweeningFeedback : MonoBehaviour, IFeedback
     [SerializeField] private float _targetScale;
     [SerializeField] private float _targetDuration;
 
+    private Tween _scaleTween;
+    private TweenCallback _resetScaleCallback;
+
+    private void Awake()
+    {
+        _resetScaleCallback = ResetScale;
+        _scaleTween = transform.DOScale(_targetScale, _targetDuration)
+                               .SetEase(Ease.Linear)
+                               .OnComplete(_resetScaleCallback)
+                               .SetAutoKill(false)
+                               .Pause();
+    }
+
     public void Play(ClickInfo clickInfo)
     {
         if (clickInfo.Type == EClickType.AutoClick) return;
-        transform.DOKill(true);
-        transform.DOScale(_targetScale, _targetDuration)
-                 .OnComplete(() => { transform.localScale = Vector3.one; })
-                 .SetEase(Ease.Linear);
+        _scaleTween.Restart();
+    }
+
+    private void ResetScale()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    private void OnDestroy()
+    {
+        _scaleTween?.Kill();
     }
 }
