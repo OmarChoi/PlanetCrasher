52b19de8fe4702d29097067a49636330e96ba240
OmarChoi <omarchoi80@gmail.com>
2026-06-01 16:04:03 +0900

    fix: Local에서 업그레이드 정보가 유지되지 않는 문제 수정

--- Assets/01.Scripts/OutGame/Feature/Upgrade/1.Repository/UpgradeEntry.cs
+++ Assets/01.Scripts/OutGame/Feature/Upgrade/1.Repository/UpgradeEntry.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using Firebase.Firestore;
+using UnityEngine;
 
 [Serializable]
 [FirestoreData]
 public class UpgradeEntry
 {
+    // JsonUtility(Local) 직렬화용 백킹 필드. Firestore는 아래 프로퍼티로 직렬화.
+    [SerializeField] private int _typeValue;
+    [SerializeField] private int _level;
+
     public EUpgradeType Type
     {
-        get => (EUpgradeType)TypeValue;
-        set => TypeValue = (int)value;
+        get => (EUpgradeType)_typeValue;
+        set => _typeValue = (int)value;
     }
 
     [FirestoreProperty]
-    public int TypeValue { get; set; }
+    public int TypeValue
+    {
+        get => _typeValue;
+        set => _typeValue = value;
+    }
 
     [FirestoreProperty]
-    public int Level { get; set; }
+    public int Level
+    {
+        get => _level;
+        set => _level = value;
+    }
 
     public UpgradeEntry() { }
 
